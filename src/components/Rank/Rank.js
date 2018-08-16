3e253b9ab3c2e0ff7874e6013e619216e1ae686f
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-08-16 14:30:47 +0530

    Rank component created and used in App.js

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Rank = props => {
+  return (
+    <div className="">
+      <div className="white f3">{'Kapil, your current rank is...'}</div>
+      <div className="white f1">{'#1'}</div>
+    </div>
+  );
+};
+
+export default Rank;
