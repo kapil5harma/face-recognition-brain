83c3e9c5a2ab05ecced1ba6e8bc02afcf6cda6c4
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-08-18 21:59:53 +0530

    Debugging process.env

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,20 +1,16 @@
 import clarifaiApiKeyProd from './clarifai.prod';
-// import clarifaiApiKeyDev from './clarifai.dev';
-
-console.log('clarifaiApiKeyProd: ', clarifaiApiKeyProd);
-// console.log('clarifaiApiKeyDev: ', clarifaiApiKeyDev);
+import clarifaiApiKeyDev from './clarifai.dev';
 
 let clarifaiApiKey;
 
+console.log('process.env: ', process.env);
 console.log('process.env.NODE_ENV: ', process.env.NODE_ENV);
+console.log('process.env.CLARIFAI_KEY: ', process.env.CLARIFAI_KEY);
 
 if (process.env.NODE_ENV === 'production') {
   clarifaiApiKey = clarifaiApiKeyProd;
-  console.log('clarifaiApiKeyProd: ', clarifaiApiKey);
+} else {
+  clarifaiApiKey = clarifaiApiKeyDev;
 }
-// else {
-//   clarifaiApiKey = clarifaiApiKeyDev;
-//   console.log('clarifaiApiKeyDev: ', clarifaiApiKey);
-// }
 
 export default clarifaiApiKey;
