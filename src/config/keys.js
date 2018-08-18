2113942b81aaed9281041cd04a101bdb4f6d3750
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-08-18 22:17:32 +0530

    Debugging to be continued, process.env.CLARIFAI_KEY is undefined

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,5 +1,5 @@
-import clarifaiApiKeyProd from './clarifai.prod';
-// import clarifaiApiKeyDev from './clarifai.dev';
+// import clarifaiApiKeyProd from './clarifai.prod';
+import clarifaiApiKeyDev from './clarifai.dev';
 
 let clarifaiApiKey;
 
@@ -7,10 +7,10 @@ console.log('process.env: ', process.env);
 console.log('process.env.NODE_ENV: ', process.env.NODE_ENV);
 console.log('process.env.CLARIFAI_KEY: ', process.env.CLARIFAI_KEY);
 
-if (process.env.NODE_ENV === 'production') {
-  clarifaiApiKey = clarifaiApiKeyProd;
-} else {
-  // clarifaiApiKey = clarifaiApiKeyDev;
-}
+// if (process.env.NODE_ENV === 'production') {
+// clarifaiApiKey = clarifaiApiKeyProd;
+// } else {
+clarifaiApiKey = clarifaiApiKeyDev;
+// }
 
 export default clarifaiApiKey;
