6b8b5c460503208958f9a38ef8951b093abeb677
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-08-18 21:48:02 +0530

    consoling NODE_ENV variable

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,11 +1,18 @@
 import clarifaiApiKeyProd from './clarifai.prod';
 import clarifaiApiKeyDev from './clarifai.dev';
+console.log('clarifaiApiKeyProd: ', clarifaiApiKeyProd);
+console.log('clarifaiApiKeyDev: ', clarifaiApiKeyDev);
 
 let clarifaiApiKey;
+
+console.log('process.env.NODE_ENV: ', process.env.NODE_ENV);
+
 if (process.env.NODE_ENV === 'production') {
   clarifaiApiKey = clarifaiApiKeyProd;
+  console.log('clarifaiApiKeyProd: ', clarifaiApiKey);
 } else {
   clarifaiApiKey = clarifaiApiKeyDev;
+  console.log('clarifaiApiKeyDev: ', clarifaiApiKey);
 }
 
 export default clarifaiApiKey;
