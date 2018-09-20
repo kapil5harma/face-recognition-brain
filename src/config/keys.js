6ce324618748da77bfea944e2c54048c7f318cd6
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-09-20 23:05:58 +0530

    dev key added to local setup

--- src/config/keys.js
+++ src/config/keys.js
@@ -3,9 +3,9 @@ import clarifaiApiKeyDev from './clarifai.dev';
 
 let clarifaiApiKey;
 
-console.log('process.env: ', process.env);
-console.log('process.env.NODE_ENV: ', process.env.NODE_ENV);
-console.log('process.env.CLARIFAI_KEY: ', process.env.CLARIFAI_KEY);
+// console.log('process.env: ', process.env);
+// console.log('process.env.NODE_ENV: ', process.env.NODE_ENV);
+// console.log('process.env.CLARIFAI_KEY: ', process.env.CLARIFAI_KEY);
 
 // if (process.env.NODE_ENV === 'production') {
 // clarifaiApiKey = clarifaiApiKeyProd;
