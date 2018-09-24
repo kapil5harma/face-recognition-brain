1b965a40ebbe37130fe7fa1b9a6051e66830be02
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-09-24 23:37:08 +0530

    Dynamic data coming via props  handled

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
 const Rank = props => {
+  const { name, entries } = props;
+
   return (
     <div className="">
-      <div className="white f3">{'Kapil, your current rank is...'}</div>
-      <div className="white f1">{'#1'}</div>
+      {`${name} , your current rank is...`}
+      <div className="white f1 ">{entries}</div>
+
+      {/* <div className="white f3">{'Kapil, your current rank is...'}</div>
+      <div className="white f1">{'#1'}</div> */}
     </div>
   );
 };
