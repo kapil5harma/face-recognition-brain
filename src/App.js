a9bca8fec3e545f9a928e7eaf3092f4565290f53
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-09-25 12:31:41 +0530

    Using image route on frontend

--- src/App.js
+++ src/App.js
@@ -85,16 +85,30 @@ class App extends Component {
     this.setState({ input: event.target.value });
   };
 
-  onButtonSubmit = () => {
+  onPictureSubmit = () => {
     this.setState(prevState => {
       return { imageURL: prevState.input };
     });
 
     app.models
       .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-      .then(response =>
-        this.displayFaceBox(this.calculateFaceLocation(response))
-      )
+      .then(response => {
+        if (response) {
+          fetch('http://localhost:3001/image', {
+            method: 'put',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+            .then(response => response.json())
+            .then(count => {
+              this.setState({ user: { ...this.state.user, entries: count } });
+            });
+        }
+
+        this.displayFaceBox(this.calculateFaceLocation(response));
+      })
       .catch(err => console.log('err: ', err));
   };
 
@@ -127,7 +141,7 @@ class App extends Component {
             />
             <ImageLinkForm
               onInputChange={this.onInputChange}
-              onButtonSubmit={this.onButtonSubmit}
+              onPictureSubmit={this.onPictureSubmit}
             />
             {imageURL ? (
               <FaceRecognition imageURL={imageURL} box={box} />
