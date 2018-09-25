70747c0db7d946a60d293681fc5ee73a5d57a2c3
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-09-25 12:31:10 +0530

    Minor change for better function name

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './ImageLinkForm.css';
 
-const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
+const ImageLinkForm = ({ onInputChange, onPictureSubmit }) => {
   return (
     <div className="ImageLinkForm">
       <p className="f3">
@@ -16,7 +16,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
           />
           <button
             className="f4 w-30 grow link ph3 pv2 dib white bg-light-purple"
-            onClick={onButtonSubmit}
+            onClick={onPictureSubmit}
           >
             Detect
           </button>
