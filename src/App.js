47a0867aa6704180ee8d8774cd3c9a0a8a45440e
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-10-22 21:43:51 +0530

    Clarifai API call removed from frontend

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
-import clarifaiApiKey from './config/keys';
-
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
@@ -12,10 +9,6 @@ import Rank from './components/Rank/Rank';
 import Register from './components/Register/Register';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import SignIn from './components/SignIn/SignIn';
-
-const app = new Clarifai.App({
-  apiKey: clarifaiApiKey
-});
 
 const particleOptions = {
   particles: {
@@ -93,8 +86,14 @@ class App extends Component {
       return { imageURL: prevState.input };
     });
 
-    app.models
-      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    fetch('http://localhost:3001/imageurl', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        input: this.state.input
+      })
+    })
+      .then(response => response.json())
       .then(response => {
         if (response) {
           fetch('http://localhost:3001/image', {
